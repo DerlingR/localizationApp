9f1d8c00e040e4cef891f2eed9664d786f80e4cd
Derling <derlingmejia07@outlook.com>
2025-01-05 12:38:45 -0400

    Test aprobado 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +10,10 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonicModule, CommonModule]
 })
 export class HomePage {
+  currentLocation: { latitude: number; longitude: number } | null = null;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {}
 
+  getLocation() {
+    this.geolocation
+      .getCurrentPosition()
+      .then((resp) => {
+        this.currentLocation = {
+          latitude: resp.coords.latitude,
+          longitude: resp.coords.longitude,
+        };
+      })
+      .catch((error) => {
+        console.error('Error getting location', error);
+      });
+  }
 }
